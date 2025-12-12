13831c0d3bcccc858957d4a46f12f639c896b7f1
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-12-12 14:02:30 +0530

    added Sum Of of Natural Numbers

---
+++ src/analysisOfAlgorithm/SumOfNaturalNumbers.java
@@ -0,0 +1,16 @@
+package analysisOfAlgorithm;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+
+        // natural number given is 10 for an example
+
+        int n = 10;
+        // there can be multiple solution to this problem
+
+        int sum = n*(n+1)/2;
+
+        System.out.println("Sum of first "+n+" natural number is = "+sum);
+
+    }
+}
